6314167de15e40e06b23d350c078ce65f461c4a2
Albina-Hais <albina.hais@yandex.by>
2025-05-14 08:11:10 +0300

    const!

--- script.js
+++ script.js
@@ -24,11 +24,7 @@ showRandomWord();
 let i = 0;
 
 document.addEventListener('keydown', function(event) {
-<<<<<<< Updated upstream
     const element = wordContainer.querySelectorAll('span');
-=======
-    let element = wordContainer.querySelectorAll('span');
->>>>>>> Stashed changes
 
     if (event.key === element[i].textContent) {
         element[i].className = 'c';
